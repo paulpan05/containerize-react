3e2dfbba431a84a0f1649cc8701340f11bdc19d3
Paul Pan <panjunhong05@gmail.com>
2019-08-07 03:08:38 -0700

    Initial change to app

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,21 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import AuthPage from './components/AuthPage';
+import { unblockPage } from './constants/ComponentFunctions';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 const App: React.FC = () => {
+  React.useEffect(() => {
+    window.addEventListener('load', unblockPage);
+    return () => {
+      window.removeEventListener('load', unblockPage);
+    }
+  }, []);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Switch>
+        <Route exact path='/' component={AuthPage} />
+      </Switch>
+    </Router>
   );
 }
 
