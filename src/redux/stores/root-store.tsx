b65e7c70eefacbfc8d7ab18b4f7c275a5df097ee
Paul Pan <panjunhong05@gmail.com>
2019-08-15 01:56:19 -0700

    Added devtools integration to store

--- src/redux/stores/root-store.tsx
+++ src/redux/stores/root-store.tsx
@@ -1,7 +1,8 @@
 import thunk from 'redux-thunk';
 import { createStore, applyMiddleware } from 'redux';
 import rootReducer from '../reducers/root-reducer';
+import { composeWithDevTools } from 'redux-devtools-extension';
 
-const rootStore = createStore(rootReducer, applyMiddleware(thunk));
+const rootStore = createStore(rootReducer, composeWithDevTools(applyMiddleware(thunk)));
 
 export default rootStore;
