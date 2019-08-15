7cfd7bb05d643995980ce6897360e4595b38ebb5
Paul Pan <panjunhong05@gmail.com>
2019-08-15 02:59:51 -0700

    Added persistence to redux

--- src/redux/stores/root-store.tsx
+++ src/redux/stores/root-store.tsx
@@ -3,6 +3,35 @@ import { createStore, applyMiddleware } from 'redux';
 import rootReducer from '../reducers/root-reducer';
 import { composeWithDevTools } from 'redux-devtools-extension';
 
-const rootStore = createStore(rootReducer, composeWithDevTools(applyMiddleware(thunk)));
+const saveStateToStorage = (state: any) => {
+  try {
+    const serializedState = JSON.stringify(state);
+    localStorage.setItem('state', serializedState);
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+const loadStateFromStorage = () => {
+  try {
+    const serializedState = localStorage.getItem('state');
+    if (serializedState === null) {
+      return undefined;
+    }
+    return JSON.parse(serializedState);
+  } catch (error) {
+    console.log(error);
+    return undefined;
+  }
+}
+
+const persistedState = loadStateFromStorage();
+
+const rootStore = createStore(
+  rootReducer,
+  persistedState,
+  composeWithDevTools(applyMiddleware(thunk)));
+
+rootStore.subscribe(() => {saveStateToStorage(rootStore.getState())})
 
 export default rootStore;
