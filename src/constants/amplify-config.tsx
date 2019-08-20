2b8e7f97352015df956993ac578c89b2b4773319
Paul Pan <panjunhong05@gmail.com>
2019-08-20 01:22:35 -0700

    Added back identity pool auth

--- src/constants/amplify-config.tsx
+++ src/constants/amplify-config.tsx
@@ -1,5 +1,6 @@
 const amplifyConfig = {
   Auth: {
+    identityPoolId: 'us-east-2:d983014d-2ae5-4b6f-b8c2-66cbd6d8abf1',
     region: 'us-east-2',
     userPoolId: 'us-east-2_7z2hKuvOk',
     userPoolWebClientId: '2q86so47ohfr6qgauntjomtoe',
