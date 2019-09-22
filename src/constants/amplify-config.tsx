a68223d646d077b7f9260b0da9d70bf527c6d075
Paul Pan <panjunhong05@gmail.com>
2019-09-22 09:58:24 -0700

    Switched AWS region to N. Virginia

--- src/constants/amplify-config.tsx
+++ src/constants/amplify-config.tsx
@@ -1,9 +1,9 @@
 export const amplifyConfig = {
   Auth: {
-    identityPoolId: 'us-east-2:d983014d-2ae5-4b6f-b8c2-66cbd6d8abf1',
-    region: 'us-east-2',
-    userPoolId: 'us-east-2_arw64WoRm',
-    userPoolWebClientId: '1drils5i5b7n081b0sb87pqcer',
+    identityPoolId: 'us-east-1:c1afc097-87c4-46b2-a015-6778eaf446dd',
+    region: 'us-east-1',
+    userPoolId: 'us-east-1_089fsCwWg',
+    userPoolWebClientId: '7agrfglqgm338f4dm8er03d01r',
     mandatorySignIn: true
   }
 }
