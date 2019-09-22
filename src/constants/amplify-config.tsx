636b4212d65c01e3a3faeeac733ca7920e0d34a3
Paul Pan <panjunhong05@gmail.com>
2019-09-22 09:42:38 -0700

    Changed to new user pool

--- src/constants/amplify-config.tsx
+++ src/constants/amplify-config.tsx
@@ -2,8 +2,8 @@ export const amplifyConfig = {
   Auth: {
     identityPoolId: 'us-east-2:d983014d-2ae5-4b6f-b8c2-66cbd6d8abf1',
     region: 'us-east-2',
-    userPoolId: 'us-east-2_7z2hKuvOk',
-    userPoolWebClientId: '2q86so47ohfr6qgauntjomtoe',
+    userPoolId: 'us-east-2_arw64WoRm',
+    userPoolWebClientId: '1drils5i5b7n081b0sb87pqcer',
     mandatorySignIn: true
   }
 }
